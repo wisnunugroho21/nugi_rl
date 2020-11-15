bcdaf9f1d78b4f19bef78ba5f13114fc548b9a7a
wisnunugroho21 <nugroho8dewantoro@gmail.com>
2020-11-15 09:47:20 +0700

    delete tf folder

--- proximal_policy_optimization/tf/policy_function/desprecated/advantage_function.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from utils.pytorch_utils import set_device
-
-def impala_advantage_estimation(rewards, values, NextReturns, dones, worker_logprobs, learner_logprobs, gamma = 0.99, lam = 0.95):
-    po          = tf.math.reduce_mean(tf.math.minimum(1.0, tf.math.exp(learner_logprobs - worker_logprobs)), 1)
-    return po * (rewards + (1.0 - dones) * gamma * NextReturns - values)
