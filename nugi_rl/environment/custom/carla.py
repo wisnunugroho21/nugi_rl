b697e4e330ff8ec2cf7c5dfa432104dc970e2389
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2021-03-28 10:25:08 +0800

    add steer to obs

--- nugi_rl/environment/custom/carla.py
+++ nugi_rl/environment/custom/carla.py
@@ -139,7 +139,7 @@ class CarlaEnv():
         del self.collision_hist[:]
         del self.crossed_line_hist[:] 
         
-        return image, np.array([0])
+        return image, np.array([0, 0])
 
     def step(self, action):
         prev_loc    = self.vehicle.get_location()
@@ -167,4 +167,4 @@ class CarlaEnv():
         if len(self.crossed_line_hist) > 0 or len(self.collision_hist) > 0 or loc.x >= -100 or loc.y >= -10 or self.cur_step >= self.max_step:
             done = True
         
-        return image, np.array([kmh]), reward, done, None
+        return image, np.array([kmh, float(steer)]), reward, done, None
