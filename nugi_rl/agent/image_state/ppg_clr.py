fix logprobs, add trpo & goal agent

nugi_rl/agent/image_state/ppg_clr.py
@@ -143,7 +143,17 @@ class AgentImageStatePPGClr(AgentPPG):
         else:
             action = self.distribution.deterministic(action_datas)
               
-        return to_list(action.squeeze(), self.use_gpu)
+        return action.squeeze().detach().tolist()
+
+    def logprobs(self, image, state, action):
+        image, state    = self.ppo_memory.transform(image).unsqueeze(0).to(self.device), torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        action          = torch.FloatTensor(action).unsqueeze(0).float().to(self.device)
+
+        res             = self.cnn(image)
+        action_datas, _ = self.policy(res, state)
+
+        logprobs        = self.distribution.logprob(action_datas, action)
+        return logprobs.squeeze().detach().tolist()
 
     def save_weights(self):
         torch.save({

nugi_rl/agent/image_state/ppg_clr_lstm.py
@@ -159,7 +159,22 @@ class AgentImageStatePPGClr(AgentPPG):
         else:
             action = self.distribution.deterministic(action_datas)
               
-        return to_list(action.squeeze(), self.use_gpu)
+        return action.squeeze().detach().tolist()
+
+    def logprobs(self, images, state, action):
+        images, state   = self.ppo_memory.transform(images).unsqueeze(0).to(self.device), torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        action          = torch.FloatTensor(action).unsqueeze(0).float().to(self.device)
+
+        batch_size, timesteps, C, H, W  = images.shape
+        images          = images.reshape(timesteps * batch_size, C, H, W)
+
+        res             = self.cnn(images)
+        res             = res.reshape(timesteps, batch_size, res.shape[-1])
+
+        action_datas, _ = self.policy(state)
+        logprobs        = self.distribution.logprob(action_datas, action)
+
+        return logprobs.squeeze().detach().tolist()
 
     def save_weights(self):
         torch.save({

nugi_rl/agent/image_state/ppg_lstm.py
@@ -108,8 +108,7 @@ class AgentImageStatePpgLstm(AgentPPG):
         self.ppo_memory.save_all(images, states, actions, rewards, dones, next_images, next_states)
 
     def act(self, images, state):
-        state               = torch.FloatTensor(state).unsqueeze(0).to(self.device)
-        images              = self.ppo_memory.transform(images).unsqueeze(0).to(self.device)
+        images, state       = self.ppo_memory.transform(images).unsqueeze(0).to(self.device), torch.FloatTensor(state).unsqueeze(0).to(self.device)
 
         batch_size, timesteps, C, H, W  = images.shape
         images              = images.reshape(timesteps * batch_size, C, H, W)
@@ -124,7 +123,22 @@ class AgentImageStatePpgLstm(AgentPPG):
         else:
             action = self.distribution.deterministic(action_datas)
               
-        return to_list(action.squeeze(), self.use_gpu)
+        return action.squeeze().detach().tolist()
+
+    def logprobs(self, images, state, action):
+        images, state   = self.ppo_memory.transform(images).unsqueeze(0).to(self.device), torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        action          = torch.FloatTensor(action).unsqueeze(0).float().to(self.device)
+
+        batch_size, timesteps, C, H, W  = images.shape
+        images          = images.reshape(timesteps * batch_size, C, H, W)
+
+        res             = self.cnn(images)
+        res             = res.reshape(timesteps, batch_size, res.shape[-1])
+
+        action_datas, _ = self.policy(state)
+        logprobs        = self.distribution.logprob(action_datas, action)
+
+        return logprobs.squeeze().detach().tolist()
 
     def save_weights(self):
         torch.save({

nugi_rl/agent/standard/ppo.py
@@ -3,17 +3,14 @@ from copy import deepcopy
 import torch
 from torch.utils.data import DataLoader
 
-class AgentPPG():  
-    def __init__(self, policy, value, distribution, ppo_loss, aux_ppg_loss, ppo_memory, aux_ppg_memory, 
-                ppo_optimizer, aux_ppg_optimizer, ppo_epochs = 10, aux_ppg_epochs = 10, n_aux_update = 10, is_training_mode = True, 
+class AgentPPO():  
+    def __init__(self, policy, value, distribution, ppo_loss, ppo_memory, ppo_optimizer, ppo_epochs = 10, is_training_mode = True, 
                 batch_size = 32,  folder = 'model', device = torch.device('cuda:0')):   
 
         self.batch_size         = batch_size  
         self.ppo_epochs         = ppo_epochs
-        self.aux_ppg_epochs     = aux_ppg_epochs
         self.is_training_mode   = is_training_mode
         self.folder             = folder
-        self.n_aux_update       = n_aux_update
 
         self.policy             = policy
         self.policy_old         = deepcopy(self.policy)
@@ -23,16 +20,11 @@ class AgentPPG():
 
         self.distribution       = distribution
         self.ppo_memory         = ppo_memory
-        self.aux_ppg_memory     = aux_ppg_memory
         
         self.ppoLoss            = ppo_loss
-        self.auxLoss            = aux_ppg_loss      
+        self.ppo_optimizer      = ppo_optimizer    
 
         self.device             = device
-        self.i_update           = 0
-
-        self.ppo_optimizer      = ppo_optimizer
-        self.aux_ppg_optimizer  = aux_ppg_optimizer
 
         if is_training_mode:
           self.policy.train()
@@ -46,10 +38,10 @@ class AgentPPG():
         return self.ppo_memory
 
     def _training_ppo(self, states, actions, rewards, dones, next_states): 
-        action_datas, _     = self.policy(states)
+        action_datas        = self.policy(states)
         values              = self.value(states)
 
-        old_action_datas, _ = self.policy_old(states, True)
+        old_action_datas    = self.policy_old(states, True)
         old_values          = self.value_old(states, True)
         next_values         = self.value(next_states, True)
 
@@ -58,18 +50,6 @@ class AgentPPG():
         self.ppo_optimizer.zero_grad()
         loss.backward()
         self.ppo_optimizer.step()
-
-    def _training_aux_ppg(self, states):  
-        action_datas, values    = self.policy(states)
-
-        returns                 = self.value(states, True)
-        old_action_datas, _     = self.policy_old(states, True)
-
-        loss = self.auxLoss.compute_loss(action_datas, old_action_datas, values, returns)
-
-        self.aux_ppg_optimizer.zero_grad()
-        loss.backward()
-        self.aux_ppg_optimizer.step()
 
     def _update_ppo(self):
         self.policy_old.load_state_dict(self.policy.state_dict())
@@ -80,31 +60,14 @@ class AgentPPG():
             for states, actions, rewards, dones, next_states in dataloader:
                 self._training_ppo(states.float().to(self.device), actions.float().to(self.device), rewards.float().to(self.device), dones.float().to(self.device), next_states.float().to(self.device))
 
-        states, _, _, _, _ = self.ppo_memory.get_all_items()
-        self.aux_ppg_memory.save_all(states)
         self.ppo_memory.clear_memory()           
 
-    def _update_aux_ppg(self):
-        self.policy_old.load_state_dict(self.policy.state_dict())
-
-        for _ in range(self.aux_ppg_epochs):
-            dataloader  = DataLoader(self.aux_ppg_memory, self.batch_size, shuffle = False, num_workers = 8)
-            for states in dataloader:
-                self._training_aux_ppg(states.float().to(self.device))
-
-        self.aux_ppg_memory.clear_memory()
-
     def update(self):
-        self._update_ppo()
-        self.i_update += 1
-
-        if self.i_update % self.n_aux_update == 0:
-            self._update_aux_ppg()
-            self.i_update = 0    
+        self._update_ppo()  
 
     def act(self, state):
         state           = torch.FloatTensor(state).unsqueeze(0).float().to(self.device)
-        action_datas, _ = self.policy(state)
+        action_datas    = self.policy(state)
         
         if self.is_training_mode:
             action = self.distribution.sample(action_datas)
@@ -117,7 +80,7 @@ class AgentPPG():
         state           = torch.FloatTensor(state).unsqueeze(0).float().to(self.device)
         action          = torch.FloatTensor(action).unsqueeze(0).float().to(self.device)
 
-        action_datas, _ = self.policy(state)
+        action_datas    = self.policy(state)
         logprobs        = self.distribution.logprob(action_datas, action)
 
         return logprobs.squeeze().detach().tolist()
@@ -130,7 +93,6 @@ class AgentPPG():
             'policy_state_dict': self.policy.state_dict(),
             'value_state_dict': self.value.state_dict(),
             'ppo_optimizer_state_dict': self.ppo_optimizer.state_dict(),
-            'aux_ppg_optimizer_state_dict': self.aux_ppg_optimizer.state_dict(),
         }, self.folder + '/ppg.tar')
         
     def load_weights(self, folder = None, device = None):
@@ -143,8 +105,7 @@ class AgentPPG():
         model_checkpoint = torch.load(self.folder + '/ppg.tar', map_location = device)
         self.policy.load_state_dict(model_checkpoint['policy_state_dict'])        
         self.value.load_state_dict(model_checkpoint['value_state_dict'])
-        self.ppo_optimizer.load_state_dict(model_checkpoint['ppo_optimizer_state_dict'])        
-        self.aux_ppg_optimizer.load_state_dict(model_checkpoint['aux_ppg_optimizer_state_dict'])
+        self.ppo_optimizer.load_state_dict(model_checkpoint['ppo_optimizer_state_dict'])
 
         if self.is_training_mode:
             self.policy.train()

nugi_rl/loss/trpo_ppo/trpo.py
@@ -1,9 +1,8 @@
 import torch
 
-class TrulyPPO():
-    def __init__(self, distribution, advantage_function, policy_kl_range = 0.0008, policy_params = 20, value_clip = 1.0, vf_loss_coef = 1.0, entropy_coef = 0.01):
+class TRPO():
+    def __init__(self, distribution, advantage_function, policy_kl_range = 0.0008, value_clip = 1.0, vf_loss_coef = 1.0, entropy_coef = 0.01):
         self.policy_kl_range    = policy_kl_range
-        self.policy_params      = policy_params
         self.value_clip         = value_clip
         self.vf_loss_coef       = vf_loss_coef
         self.entropy_coef       = entropy_coef
@@ -11,6 +10,7 @@ class TrulyPPO():
         self.advantage_function = advantage_function
         self.distribution       = distribution
 
+    # Loss for PPO  
     def compute_loss(self, action_datas, old_action_datas, values, old_values, next_values, actions, rewards, dones):
         advantages      = self.advantage_function.compute_advantages(rewards, values, next_values, dones).detach()
         returns         = (advantages + values).detach()
@@ -18,14 +18,16 @@ class TrulyPPO():
         logprobs        = self.distribution.logprob(action_datas, actions) + 1e-5
         old_logprobs    = (self.distribution.logprob(old_action_datas, actions) + 1e-5).detach()
 
-        ratios          = (logprobs - old_logprobs).exp()       
-        Kl              = self.distribution.kldivergence(old_action_datas, action_datas) + 1e-5
+        ratios          = (logprobs - old_logprobs).exp()
+        Kl              = self.distribution.kldivergence(action_datas, old_action_datas)
+        zeros           = torch.zeros_like(advantages, dtype = torch.float32)
 
-        pg_targets  = torch.where(
-            (Kl >= self.policy_kl_range) & (ratios > 1.0),
-            ratios * advantages - self.policy_params * Kl,
-            ratios * advantages
+        pg_targets      = torch.where(
+            Kl <= self.policy_kl_range,
+            ratios * advantages,
+            zeros
         )
+
         pg_loss         = pg_targets.mean()
         dist_entropy    = self.distribution.entropy(action_datas).mean()
 
@@ -35,5 +37,5 @@ class TrulyPPO():
             vpredclipped    = old_values + torch.clamp(values - old_values, -self.value_clip, self.value_clip)
             critic_loss     = ((returns - vpredclipped).pow(2) * 0.5).mean()
 
-        loss = (critic_loss * self.vf_loss_coef) -  (dist_entropy * self.entropy_coef) - pg_loss
+        loss = (critic_loss * self.vf_loss_coef) - (dist_entropy * self.entropy_coef) - pg_loss
         return loss

nugi_rl/model/ppo/TanhStdNN.py
@@ -9,7 +9,7 @@ class Policy_Model(nn.Module):
         self.nn_layer = nn.Sequential(
           nn.Linear(state_dim, 256),
           nn.ReLU(),
-          nn.Linear(256, 192),
+          nn.Linear(256, 128),
           nn.ReLU(),
         )
 
@@ -21,22 +21,17 @@ class Policy_Model(nn.Module):
           nn.Linear(64, action_dim),
           nn.Sigmoid()
         )
-
-        self.critic_layer = nn.Sequential(
-          nn.Linear(64, 1)
-        )
         
     def forward(self, states, detach = False):
       x = self.nn_layer(states)
 
       mean    = self.actor_mean_layer(x[:, :64])
       std     = self.actor_std_layer(x[:, 64:128])
-      critic  = self.critic_layer(x[:, 128:192])
       
       if detach:
-        return (mean.detach(), std.detach()), critic.detach()
+        return (mean.detach(), std.detach())
       else:
-        return (mean, std), critic
+        return (mean, std)
       
 class Value_Model(nn.Module):
     def __init__(self, state_dim):