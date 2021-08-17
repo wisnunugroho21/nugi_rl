21129d9336738e5d7413b653f173540bcc138ec8
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2021-08-17 07:39:48 +0700

    add new examples

--- nugi_rl/model/ppg/TanhStdNN.py
+++ nugi_rl/model/ppg/TanhStdNN.py
@@ -7,31 +7,31 @@ class Policy_Model(nn.Module):
         super(Policy_Model, self).__init__()
 
         self.nn_layer = nn.Sequential(
-          nn.Linear(state_dim, 256),
+          nn.Linear(state_dim, 1024),
           nn.ReLU(),
-          nn.Linear(256, 192),
+          nn.Linear(1024, 768),
           nn.ReLU(),
         )
 
         self.actor_mean_layer = nn.Sequential(
-          nn.Linear(64, action_dim)
+          nn.Linear(256, action_dim)
         )
 
         self.actor_std_layer = nn.Sequential(
-          nn.Linear(64, action_dim),
+          nn.Linear(256, action_dim),
           nn.Sigmoid()
         )
 
         self.critic_layer = nn.Sequential(
-          nn.Linear(64, 1)
+          nn.Linear(256, 1)
         )
         
     def forward(self, states, detach = False):
       x = self.nn_layer(states)
 
-      mean    = self.actor_mean_layer(x[:, :64])
-      std     = self.actor_std_layer(x[:, 64:128])
-      critic  = self.critic_layer(x[:, 128:192])
+      mean    = self.actor_mean_layer(x[:, :256])
+      std     = self.actor_std_layer(x[:, 256:512])
+      critic  = self.critic_layer(x[:, 512:768])
       
       if detach:
         return (mean.detach(), std.detach()), critic.detach()
@@ -43,11 +43,11 @@ class Value_Model(nn.Module):
         super(Value_Model, self).__init__()   
 
         self.nn_layer = nn.Sequential(
-          nn.Linear(state_dim, 256),
+          nn.Linear(state_dim, 1024),
           nn.ReLU(),
-          nn.Linear(256, 64),
+          nn.Linear(1024, 256),
           nn.ReLU(),
-          nn.Linear(64, 1)
+          nn.Linear(256, 1)
         )
         
     def forward(self, states, detach = False):
