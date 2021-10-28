e111e1e5509dd294389aef6cb6eda8913c91dfcb
Nugroho Dewantoro <d.nugroho@idmarco.co.id>
2021-10-28 07:57:09 +0700

    update a2c, ppo_kl, ppo_clip

--- nugi_rl/loss/a2c.py
+++ nugi_rl/loss/a2c.py
@@ -2,15 +2,12 @@ import torch
 from torch import Tensor
 
 from nugi_rl.distribution.base import Distribution
-from nugi_rl.policy_function.advantage_function.generalized_advantage_estimation import GeneralizedAdvantageEstimation
 
-class A2C(A2C):
-    def __init__(self, distribution: Distribution, advantage_function: GeneralizedAdvantageEstimation):
-        self.advantage_function = advantage_function
+class A2C():
+    def __init__(self, distribution: Distribution):
         self.distribution       = distribution
  
-    def compute_loss(self, action_datas: tuple, values: Tensor, next_values: Tensor, actions: Tensor, rewards: Tensor, dones: Tensor) -> Tensor:
-        advantages      = self.advantage_function.compute_advantages(rewards, values, next_values, dones).detach()
+    def compute_loss(self, action_datas: tuple, actions: Tensor, advantages: Tensor) -> Tensor:
         logprobs        = self.distribution.logprob(action_datas, actions) + 1e-5
 
         pg_target       = logprobs * advantages
--- nugi_rl/loss/ppo/ppo_kl.py
+++ nugi_rl/loss/ppo/ppo_kl.py
@@ -2,21 +2,14 @@ import torch
 from torch import Tensor
 
 from nugi_rl.distribution.base import Distribution
-from nugi_rl.policy_function.advantage_function.generalized_advantage_estimation import GeneralizedAdvantageEstimation
 from nugi_rl.loss.ppo.base import Ppo
 
 class PpoKl(Ppo):
-    def __init__(self, distribution: Distribution, advantage_function: GeneralizedAdvantageEstimation, 
-        policy_params: float = 1):
-
+    def __init__(self, distribution: Distribution, policy_params: float = 1):
         self.policy_params  = policy_params
-
-        self.advantage_function = advantage_function
         self.distribution       = distribution
  
-    def compute_loss(self, action_datas: tuple, old_action_datas: tuple, values: Tensor, next_values: Tensor, actions: Tensor, rewards: Tensor, dones: Tensor) -> Tensor:
-        advantages      = self.advantage_function.compute_advantages(rewards, values, next_values, dones).detach()
-
+    def compute_loss(self, action_datas: tuple, old_action_datas: tuple, actions: Tensor, advantages: Tensor) -> Tensor:
         logprobs        = self.distribution.logprob(action_datas, actions) + 1e-5
         old_logprobs    = (self.distribution.logprob(old_action_datas, actions) + 1e-5).detach()
 
