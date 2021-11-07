de1a24f4bf312bc4ad189a2255ddaaa071cbe69f
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2021-11-07 15:33:22 +0700

    fix device in executor

--- nugi_rl/train/executor/sync.py
+++ nugi_rl/train/executor/sync.py
@@ -1,14 +1,17 @@
 import time
 import datetime
 import ray
+import torch
 
 from nugi_rl.train.runner.base import Runner
 from nugi_rl.train.executor.base import Executor
 from nugi_rl.agent.base import Agent
 
+from torch import device
+
 class SyncExecutor(Executor):
     def __init__(self, agent: Agent, n_iteration: int, runner: Runner, save_weights: bool = False, 
-        n_saved: int = 10, load_weights: bool = False, is_training_mode: bool = True):
+        n_saved: int = 10, load_weights: bool = False, is_training_mode: bool = True, learner_device: device = torch.device('cuda')):
 
         self.agent              = agent
         self.runner             = runner
@@ -18,6 +21,7 @@ class SyncExecutor(Executor):
         self.n_saved            = n_saved
         self.is_training_mode   = is_training_mode 
         self.load_weights       = load_weights
+        self.learner_device     = learner_device
         
     def execute(self):
         if self.load_weights:
@@ -39,6 +43,8 @@ class SyncExecutor(Executor):
                         memory, _ = data
 
                         states, actions, rewards, dones, next_states, logprobs = memory
+                        states, actions, rewards, dones, next_states, logprobs = states.to(self.learner_device), actions.to(self.learner_device), rewards.to(self.learner_device), dones.to(self.learner_device), next_states.to(self.learner_device), logprobs.to(self.learner_device)
+                        
                         self.agent.save_all(states, actions, rewards, dones, next_states, logprobs)
                 
                     self.agent.update()
