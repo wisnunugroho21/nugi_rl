e42de0039a4c9fc4bc0b86c244bd55eacd0b7576
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2022-01-22 14:35:09 +0700

    revert

--- nugi_rl/agent/image/ppo.py
+++ nugi_rl/agent/image/ppo.py
@@ -10,17 +10,12 @@ from nugi_rl.distribution.base import Distribution
 from nugi_rl.loss.ppo.base import Ppo
 from nugi_rl.loss.value import ValueLoss
 from nugi_rl.loss.entropy import EntropyLoss
-from nugi_rl.memory.policy.base import PolicyMemory
+from nugi_rl.memory.policy.image import ImagePolicyMemory
 
 class AgentImagePPO(AgentPPO):  
-    def __init__(self, policy: Module, value: Module, distribution: Distribution, policy_loss: Ppo, value_loss: ValueLoss, entropy_loss: EntropyLoss, memory: PolicyMemory, optimizer: Optimizer, trans: Compose, ppo_epochs: int = 10, is_training_mode: bool = True, batch_size: int = 32, folder: str = 'model', device: device = torch.device('cuda'), policy_old: Module = None, value_old: Module = None):
+    def __init__(self, policy: Module, value: Module, distribution: Distribution, policy_loss: Ppo, value_loss: ValueLoss, entropy_loss: EntropyLoss, memory: ImagePolicyMemory, optimizer: Optimizer, trans: Compose, ppo_epochs: int = 10, is_training_mode: bool = True, batch_size: int = 32, folder: str = 'model', device: device = torch.device('cuda'), policy_old: Module = None, value_old: Module = None):
         super().__init__(policy, value, distribution, policy_loss, value_loss, entropy_loss, memory, optimizer, ppo_epochs=ppo_epochs, is_training_mode=is_training_mode, batch_size=batch_size, folder=folder, device=device, policy_old=policy_old, value_old=value_old)
         self.trans = trans
-
-    def _update_step(self, states: Tensor, actions: Tensor, rewards: Tensor, dones: Tensor, next_states: Tensor) -> None:
-        states      = self.trans(states)
-        next_states = self.trans(next_states)
-        return super()._update_step(states, actions, rewards, dones, next_states)
 
     def act(self, state: Tensor) -> Tensor:
         with torch.inference_mode():
